8e21f89e137e415d888165fd1073fc5dbc77d438
Angela-Sutton <angela.sutton1999@gmail.com>
2018-11-27 13:31:12 -0500

    added side bar in flexbox

--- map-app-project-7/src/App.js
+++ map-app-project-7/src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import SideBar  from './components/SideBar'
 
 class App extends Component {
 
+componentDidMount = () => {
+  this.createMap();
+}
+
+createMap = () => {
+  scriptLoader ('https://maps.googleapis.com/maps/api/js?key=AIzaSyDy8YsvWWMMNGGHvIincdacAy34zKYKUN0&callback=initMap');
+  window.initMap = this.initMap;
+}
+
 initMap =() => {
   let map = new  window.google.maps.Map(document.getElementById('map'),
-   {center: { lat:27.948460, lng: -80.663460} , zoom:14});
+   {center: { lat:27.949515, lng: -80.658322} , zoom:16});
     }
+
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-        </header>
-      </div>
-    );
+      <main>
+      <SideBar/>
+      <div id = 'map'> </div>
+    </main>
+);
   }
+}
+
+ function scriptLoader(url) {
+  let index = window.document.getElementsByTagName('script')[0];
+  let script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
